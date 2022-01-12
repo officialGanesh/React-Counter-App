2f034df7d47a4840819fd443ac77145d6846fa1b
Ganesh <ganuofficial2795@gmail.com>
2022-01-12 21:09:20 +0530

    Logic-added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0)
+
+  // Decrease count
+  const decreaseCount = () => {
+    setCount(count-1)
+  }
+  // Reset count to 0
+  const resetCount = () => {setCount(0)}
+  // Increase count
+  const increaseCount = () => {setCount(count+1)}
+
   return (
     <>
         
-    
+    <div className="container">
+
+      <div id="counter-value">
+        <h1>{count}</h1>
+      </div>
+
+      <div>
+        <button className='btn btn-success mx-3' onClick={decreaseCount}>Decrease</button>
+        <button className='btn btn-primary mx-3' onClick={resetCount}>Reset</button>
+        <button className='btn btn-danger mx-3' onClick={increaseCount}>Increase</button>
+      </div>
+
+    </div>
     
     </>
   );
