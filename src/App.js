21b590c4f808143b97fc9fa1624ebc0afa238196
Ganesh <ganuofficial2795@gmail.com>
2022-01-13 12:31:14 +0530

    Style-added

--- src/App.js
+++ src/App.js
@@ -14,19 +14,32 @@ function App() {
   // Increase count
   const increaseCount = () => {setCount(count+1)}
 
+  const myStyle = {
+      maxWidth:"55rem",
+      marginTop:"16rem",
+      marginLeft:"30rem",
+      textAlign:"center"
+
+  }
+
+  const styleValue = {
+    marginBottom:"3rem",
+    fontSize:"8rem"
+  }
+
   return (
     <>
         
-    <div className="container">
+    <div className="container" style={myStyle}>
 
       <div id="counter-value">
-        <h1>{count}</h1>
+        <h1 style={styleValue}>{count}</h1>
       </div>
 
       <div>
-        <button className='btn btn-success mx-3' onClick={decreaseCount}>Decrease</button>
-        <button className='btn btn-primary mx-3' onClick={resetCount}>Reset</button>
-        <button className='btn btn-danger mx-3' onClick={increaseCount}>Increase</button>
+        <button className='btn btn-success mx-4' onClick={decreaseCount}>Decrease</button>
+        <button className='btn btn-primary mx-4' onClick={resetCount}>Reset</button>
+        <button className='btn btn-danger mx-4' onClick={increaseCount}>Increase</button>
       </div>
 
     </div>
